f9b83597809c794b968a941d548099106f963e89
pmknyc <pmknyc@optimum.net>
2019-07-16 15:59:35 -0400

    20190716-0358 sketch UX plan in CLI file

---
+++ lib/science_daily/cli.rb
@@ -0,0 +1,21 @@
+
+class CLI
+
+# UX plan:
+# Startup - 
+#   Welcome message
+#   Brief description of app
+#   Presents user a list of all Top Science headlines (titles)
+#     Bonus: ?? also shows user Date headlines updated (must Add to scrape)
+#     Bonus: ?? user option to order headline list 
+#             in alpha by title or date order newest 1st 
+
+# User Options
+#   Ask user to choose headline to see more info
+#     Number the list 
+#     ?? OR user enters 1st 3 to 5 chars in title (fun to code this)
+
+# Pseudocode / Stubs
+  
+
+end # class CLI end
