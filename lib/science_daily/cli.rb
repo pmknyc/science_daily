f043a8363daa19ea4ae2cf2541567ac458ff117f
pmknyc <pmknyc@optimum.net>
2019-08-05 20:25:37 -0400

    add heredocs for start text & article choice

--- lib/science_daily/cli.rb
+++ lib/science_daily/cli.rb
@@ -7,25 +7,29 @@ class ScienceDaily::CLI
 
   def start
     # tells user what to do
-    start_heredoc
-    input_start = gets.strip.downcase
-    case input_start
-      when 'h'
-        # call Article class method to run first scrape
-        ScienceDaily::Article.get_articles_list  
-        ScienceDaily::Article.all.each.with_index(1) do |a, i| 
-          unless i == 10
-            puts " #{i}. #{a.title}"
-          else 
-            puts "#{i}. #{a.title}"
-          end
+    start_doc
+    # Display headlines: call Article methods to run first scrape
+      ScienceDaily::Article.get_articles_list  
+      
+      ScienceDaily::Article.all.each.with_index(1) do |a, i| 
+        unless i == 10
+          puts " #{i}. #{a.title}"
+        else 
+          puts "#{i}. #{a.title}"
         end
-      when 'e' || 'exit'
-        exit
-      else
-        puts "I don't understand that. Please try again."
-        start  # recurse
-    end
+      end
+    choose_or_exit_doc
+    input_choose = gets.strip.downcase
+   # case input_choose
+   #   when (/[1-10]/)
+   #   
+   #     end
+   #   when 'e' || 'exit'
+   #     exit
+   #   else
+   #     puts "I don't understand that. Please try again."
+   #     start  # recurse
+   # end
   end
   
 # ask user to choose an article
@@ -36,19 +40,25 @@ class ScienceDaily::CLI
 #   with later versions of app
 
 # heredoc to use at initial start of app
-  def start_heredoc
-    <<~HEREDOC
+  def start_doc
+    puts <<~WELCOME
+
                     Welcome to Science Daily News
       
       Breaking news headlines in science from around the world!
       Updated:  ??update time here??
 
+    WELCOME
+  end
+      
+  def choose_or_exit_doc
+    puts <<~CHOICE
       To learn more, enter the number of any headline that fascinates you.
       To exit the application, type "e" or "exit".
    
-    HEREDOC
+      CHOICE
   end
-       
+
   def again_heredoc
     # ScienceDaily.com 
     # To continue, type a choice and press <Enter>\n" 
