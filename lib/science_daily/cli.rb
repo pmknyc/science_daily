56e163093e6f797980f5ee323a24f7000a6e1bec
pmknyc <pmknyc@optimum.net>
2019-08-19 13:47:33 -0400

    better manage user article choice; use_choice & find_chosen_article methods

--- lib/science_daily/cli.rb
+++ lib/science_daily/cli.rb
@@ -1,30 +1,28 @@
 class ScienceDaily::CLI
 
+  attr_accessor :choice
+  
   def self.start
     start_doc # Welcome message heredoc
     list_articles # Call methods to scrape, create article objects, display article headlines list
+    choose_or_exit_doc
     get_user_choice # returns: article number chosen OR goodbye message + exit  
+    find_chosen_article
+    ScienceDaily::Article.add_article_features # call Article.add_article_details
     # display_chosen_article # call methods for 2nd scrape, chosen article's URL, add feature attributes, display details to user
   
-  # hard code scrape call for testing - refactor to call local method or Article method
-    ScienceDaily::Article.add_article_features # call Article.add_article_details
+      # hard code scrape call for testing - refactor to call local method or Article method
+ 
   end 
 
   def self.list_articles
     ScienceDaily::Article.create_articles # 1st scrape, make article objects   
     # now iterate all article objects to display list for user
-    ScienceDaily::Article.all.collect.with_index(1) do |a, i| 
-      unless i > 9
-        puts " #{i}. #{a.title}"
-      else 
-        puts "#{i}. #{a.title}"
-      end
-    end
+    ScienceDaily::Article.list_articles
   end
 
   def self.get_user_choice
-    choose_or_exit_doc 
-            p 'in CLI#get_user_choice method'
+       p 'in CLI#get_user_choice method'
     input = gets.strip # gets User's article choice || exit
     text_input = ['e', 'ex', 'ext','exit'].include?(input.downcase) # 'exit' app options allow typo errors
     digit_input = (1..10).include?(input.to_i) # number choices; 10 articles in "latest headlines" list
@@ -38,14 +36,24 @@ class ScienceDaily::CLI
         puts "I don't understand that. Please try again."
         get_user_choice # recurse until valid user choice
     end
-    choice
+    @@choice = choice - 1 # set class var of article choice, as array index value
   end
 
+  def self.use_choice
+    @@choice
+  end
+
+  def self.find_chosen_article
+    p "in CLI.find_chosen_article"
+    ScienceDaily::Article.all[self.use_choice] # finds article by index in Article.all
+  end
+#  helper below didn't work, I set up process flow wrong.
+#  replace with new CLI finder method above
 # #choice_index -- Helper
 # call from Article class to help find chosen article in Article.all
-  def self.choice_index
-    article_index = get_user_choice - 1
-  end
+#  def self.choice_index
+#    article_index = get_user_choice - 1
+#  end
 
  def self.display_chosen_article
               p "in CLI #display_chosen_article"
